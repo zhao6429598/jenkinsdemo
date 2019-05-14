515ff20168dfc08861c8ff4088a028d4855b6c8e
zhao6429598 <348088053@qq.com>
2019-05-14 16:16:27 +0800

    maven-java-demo

---
+++ src/main/java/com/itqf/demo/JavaPrint.java
@@ -0,0 +1,16 @@
+package com.itqf.demo;
+
+import java.sql.SQLOutput;
+
+public class JavaPrint {
+
+
+    public  static  void  main(String [] args){
+
+
+        System.out.println("hello world");
+
+    }
+
+
+}
